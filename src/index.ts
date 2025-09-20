9c44e00687b7ba77374b778b45038d148e7af2b0
felipe garzon <felipegarxon@hotmail.com>
2025-09-20 00:04:55 -0500

    :+1:  test database lib

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import { Advertiser, Database, Role } from 'marketing-request-database-lib';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+  Database.init({
+    host: 'localhost',
+    database: 'marketing_request_db',
+    port: 3306,
+    username: 'marketing_user',
+    password: 'MarketingApp25@',
+    dialect: "mysql"
+  });
+  Database.getConnection();
+  const roles = await Role.findAll();
+  console.log("ROLES: ", roles);
+  res.status(200).json(roles).end();
 });
 
 app.listen(port, () => {
